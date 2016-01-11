41a3e2bb06f49c9e207715bfdb20b263ced36930
Eric Leung <etleung21@gmail.com>
2016-01-10 18:42:36 -0800

    create.sql: update description and add semicolon

--- postgresql/create.sql
+++ postgresql/create.sql
@@ -1,9 +1,9 @@
--- create a table
+-- create a universities table
 CREATE TABLE universities (
     name varchar(20) PRIMARY KEY,
     country varchar(20),
     state varchar(20),
     city varchar(20),
-    motto varchar(50)
+    motto varchar(50),
     established int
-)
+);
